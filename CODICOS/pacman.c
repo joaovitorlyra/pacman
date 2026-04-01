189d2f549b36a8c611ae4eac473f9a437fd19f80
João Vitor Lyra <jlyra187731@gmail.com>
2026-04-01 10:42:00 -0300

    Add movement and map display functions in pacman.c

--- CODICOS/pacman.c
+++ CODICOS/pacman.c
@@ -36,13 +36,63 @@ alocamapa();
     fclose (f);
 }
 
+void imprememapa(){
+    for (int i = 0; i < 5; i++){
+        printf("%s\n", mapa[i]);
+    }
+}
+int acabou(){
+    return 0;
+}
+
+void move (char diercao){
+    int x;
+    int y;
+
+for (int i = 0; i < linhas; i++){
+    for (int j = 0; j < colunas; j++){
+        if (mapa [i][j] == '@'){
+            x = i;
+            y = j;
+            break;
+        }
+    }
+}
+
+switch (diercao){
+    case 'a':
+    mapa[x] [y-1] = '@';
+        break;
+
+    case 'w':
+    mapa[x-1] [y] = '@';
+        break;
+
+    case 's':
+    mapa [x+1] [y] = '@';
+        break;
+
+    case 'd':
+    mapa [x] [y+1] = '@';
+    break;
+  }
+
+}
+
 int main(){
 
 lermapa();     
 
-    for (int i = 0; i < 5; i++){
-        printf("%s\n", mapa[i]);
-    }
+do{
+    imprememapa();
+
+    char comando;
+    scanf( "%c", &comando);
+    move(comando);
+
+} while (acabou());
+
+    
     
 liberamapa();
 
